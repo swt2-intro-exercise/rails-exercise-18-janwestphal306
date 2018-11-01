0f1a4f2245e69311d81d742dfc4295461776fc84
Jan Westphal <jan.westphal306@gmail.com>
2018-11-01 22:40:38 +0100

    Fulfill #4

---
+++ app/models/author.rb
@@ -0,0 +1,13 @@
+class Author < ApplicationRecord
+  attr_reader :first_name, :last_name, :homepage
+
+  def initialize(params)
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @homepage = params[:homepage]
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Author Model', type: :model do
 
-  it 'should do something' do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
-    expect(author.first_name).to eq('Alan')
+  it 'should create an object' do
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+    expect(author.first_name).to eq("Alan")
+    expect(author.name).to eq("Alan Turing")
   end
 end
