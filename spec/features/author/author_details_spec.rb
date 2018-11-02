c63c298126e1166d24aa61ff4ce435a8e88c61cc
Jan Westphal <jan.westphal306@gmail.com>
2018-11-02 15:52:24 +0100

    Add test and fulfill #13

--- spec/features/author/author_details_spec.rb
+++ spec/features/author/author_details_spec.rb
@@ -8,4 +8,10 @@ describe 'Author Details Page', type: :feature do
     expect(page).to have_text("Turing")
     expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
+
+  it 'should have a link to the edit page' do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_link "Edit", href: edit_author_path(@alan)
+  end
 end
