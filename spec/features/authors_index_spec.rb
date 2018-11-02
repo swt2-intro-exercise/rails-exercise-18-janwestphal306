a2e278f1e4373c101ba28b5797705966d0e65aac
Jan Westphal <jan.westphal306@gmail.com>
2018-11-02 10:41:27 +0100

    Failing test for #8

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -5,4 +5,13 @@ describe "Authors Index Page", type: :feature do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
   end
+
+  it "should display authors" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_text "Alan"
+    expect(page).to have_text "Turing"
+    expect(page).to have_text "http://wikipedia.de/Alan_Turing"
+    expect(page).to have_link "Show", href: author_path(@alan)
+  end
 end
