76f53b6dd61888a025f4403040692676976cc2db
Jan Westphal <jan.westphal306@gmail.com>
2018-11-02 15:29:11 +0100

    Failing test for #12

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -5,4 +5,14 @@ describe 'Author Edit Page' do
     @alan = FactoryBot.create :author
     visit edit_author_path(@alan)
   end
+
+  it 'should change the authors firstname' do
+    @alan = FactoryBot.create :author
+    visit edit_author_path(@alan)
+
+    fill_in 'author[first_name', with: 'Alan Mathison'
+    find('input[type="submit"]').click
+
+    expect(@alan.first_name).to eq('Alan Mathison')
+  end
 end
