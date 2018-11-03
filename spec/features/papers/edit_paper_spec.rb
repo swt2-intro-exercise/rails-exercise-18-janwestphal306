4e2864c28446f3cf70fa2b37f6d5d3f59f221d3b
Jan Westphal <jan.westphal306@gmail.com>
2018-11-03 16:04:50 +0100

    Failing test for #21

--- spec/features/papers/edit_paper_spec.rb
+++ spec/features/papers/edit_paper_spec.rb
@@ -7,4 +7,19 @@ describe 'Paper Edit Page' do
 
     expect(page).to have_css('select[multiple]')
   end
+
+  it 'should update selected authors for paper authors' do
+    @alan = FactoryBot.create :author
+    @peter = FactoryBot.create(:author, first_name: "Peter", last_name: "Plagitarius")
+    @paper = FactoryBot.create(:paper, authors: [@alan])
+    visit edit_paper_path(@paper)
+
+    @select_authors = find "select"
+    @select_authors.select @peter.name
+
+    find('input[type="submit"]').click
+    @paper.reload
+
+    expect(@paper.authors).to include(@peter)
+  end
 end
